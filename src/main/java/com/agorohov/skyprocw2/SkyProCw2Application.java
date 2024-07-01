2d91ac7f9cfd2e193a44b0fa3d3c137eb19333e1
agorohov <aleksandr.gorohov@gmail.com>
2024-07-01 13:38:52 +0300

    initial commit

---
+++ src/main/java/com/agorohov/skyprocw2/SkyProCw2Application.java
@@ -0,0 +1,13 @@
+package com.agorohov.skyprocw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyProCw2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyProCw2Application.class, args);
+    }
+
+}
