2b03d47b47597f9967784eab5e7a87a9041d8428
agorohov <aleksandr.gorohov@gmail.com>
2024-07-05 17:10:32 +0300

    приступил к тестам сервисов

---
+++ src/test/java/com/agorohov/skyprocw2examservice/service/ExaminerServiceImplTest.java
@@ -0,0 +1,10 @@
+package com.agorohov.skyprocw2examservice.service;
+
+import org.junit.jupiter.api.Test;
+
+class ExaminerServiceImplTest {
+
+    @Test
+    void getQuestions() {
+    }
+}
---
+++ src/test/java/com/agorohov/skyprocw2examservice/service/JavaQuestionServiceTest.java
@@ -0,0 +1,26 @@
+package com.agorohov.skyprocw2examservice.service;
+
+import org.junit.jupiter.api.Test;
+
+class JavaQuestionServiceTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void testAdd() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getRandomQuestion() {
+    }
+}
