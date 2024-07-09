71aecd2117b0a7a1a43418e9d5b05ce7ed22026f
agorohov <aleksandr.gorohov@gmail.com>
2024-07-09 18:53:22 +0300

    getQuestions() покрасивее

--- src/main/java/com/agorohov/skyprocw2examservice/service/ExaminerServiceImpl.java
+++ src/main/java/com/agorohov/skyprocw2examservice/service/ExaminerServiceImpl.java
@@ -5,11 +5,12 @@ import com.agorohov.skyprocw2examservice.model.Question;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class ExaminerServiceImpl implements ExaminerService {
-
     private final List<QuestionService> questionServices;
+
     private final Random random;
 
     public ExaminerServiceImpl(List<QuestionService> questionServices, Random random) {
@@ -17,35 +18,27 @@ public class ExaminerServiceImpl implements ExaminerService {
         this.random = random;
     }
 
-
     @Override
     public Collection<Question> getQuestions(int amount) {
-        // Получаем общее кол-во вопросов из всех сервисов.
-        // Можно было бы выгрузить сразу все вопросы в список и работать с ним, но по заданию надо
-        // использовать getRandomQuestion() из QuestionService
-        int totalQuestionsAmount = questionServices.stream()
+        // Получаем список непустых сервисов
+        List<QuestionService> servicesWithQuestions = questionServices.stream()
+                .filter(Objects::nonNull)
+                .filter((e) -> !e.getAll().isEmpty())
+                .collect(Collectors.toCollection(ArrayList::new));
+        // Получаем общее кол-во вопросов из всех сервисов
+        int totalQuestionsAmount = servicesWithQuestions.stream()
                 .mapToInt((e) -> e.getAll().size())
                 .sum();
         // Проверяем, в допустимых ли пределах amount
         if (amount > totalQuestionsAmount || amount < 1) {
             throw new NotCorrectAmountQuestionsGetFromRepositoryException("Некорректное количество вопросов");
         }
-        // Инициализируем множество, куда будем складывать рандомные вопросы
+        // Инициализируем множество, куда будем складывать уникальные рандомные вопросы
         Set<Question> result = new HashSet<>();
-        // Получаем список индексов сервисов, в которых есть хотя бы 1 вопрос
-        // (чтобы не запрашивать вопрос у пустых сервисов)
-        List<Integer> indexesOfServicesWithQuestions = new ArrayList<>();
-        for (int i = 0; i < questionServices.size(); i++) {
-            if (questionServices.get(i) != null && !questionServices.get(i).getAll().isEmpty()) {
-                indexesOfServicesWithQuestions.add(i);
-            }
-        }
         // Цикл получения случайных вопросов
         while (result.size() < amount) {
-            // Получаем индекс сервиса
-            int randServiceIndex = random.nextInt(indexesOfServicesWithQuestions.size());
             // Получаем случайный вопрос и добавляем в множество
-            result.add(questionServices.get(randServiceIndex).getRandomQuestion());
+            result.add(servicesWithQuestions.get(random.nextInt(servicesWithQuestions.size())).getRandomQuestion());
         }
         return result;
     }
